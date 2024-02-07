55a13a92e69d80f5c767842f3f76e9eec473eb19
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-07 15:24:44 +0200

    Update NumberService

--- app/src/main/java/com/example/funnumberfacts/network/numberservice/NumberService.kt
+++ app/src/main/java/com/example/funnumberfacts/network/numberservice/NumberService.kt
@@ -1,14 +1,17 @@
 package com.example.funnumberfacts.network.numberservice
 
+import com.example.funnumberfacts.data.NumberFact
+import com.example.funnumberfacts.network.response.toNumberFact
+
 interface NumberService {
-    suspend fun getFact(number: Int): String
-    suspend fun getRandomFact(): String
+    suspend fun getFact(number: Int): NumberFact
+    suspend fun getRandomFact(): NumberFact
 }
 
 class NumberServiceImpl(private val service: NumberRetrofitService) : NumberService {
-    override suspend fun getFact(number: Int): String =
-        service.getFact(number)
+    override suspend fun getFact(number: Int): NumberFact =
+        service.getFact(number).toNumberFact()
 
-    override suspend fun getRandomFact(): String =
-        service.getRandomFact()
+    override suspend fun getRandomFact(): NumberFact =
+        service.getRandomFact().toNumberFact()
 }
