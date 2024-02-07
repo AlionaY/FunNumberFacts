8c502de363436a5d11c880eaf0c75131f2df9282
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-07 15:21:46 +0200

    Make response model

---
+++ app/src/main/java/com/example/funnumberfacts/network/response/FactResponse.kt
@@ -0,0 +1,24 @@
+package com.example.funnumberfacts.network.response
+
+import com.example.funnumberfacts.data.FactType
+import com.example.funnumberfacts.data.NumberFact
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FactResponse(
+    @SerialName("text")
+    val fact: String,
+    @SerialName("number")
+    val number: Int,
+    @SerialName("found")
+    val found: Boolean,
+    @SerialName("type")
+    val type: FactType
+)
+
+fun FactResponse.toNumberFact() =
+    NumberFact(
+        number = number,
+        fact = fact
+    )
