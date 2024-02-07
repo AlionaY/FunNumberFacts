bd3a333c5908f981169d18c30a5ba67715fb0648
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-07 21:25:04 +0200

    Fix NumberFactRepository

--- app/src/main/java/com/example/funnumberfacts/repository/NumberFactRepository.kt
+++ app/src/main/java/com/example/funnumberfacts/repository/NumberFactRepository.kt
@@ -4,31 +4,59 @@ import com.example.funnumberfacts.data.NumberFact
 import com.example.funnumberfacts.db.FactItem
 import com.example.funnumberfacts.db.NumberFactDao
 import com.example.funnumberfacts.db.toNumberFact
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 interface NumberFactRepository {
-    suspend fun getHistory(): List<NumberFact>
-    suspend fun getFactById(id: Int): NumberFact
-    suspend fun addFactToHistory(item: NumberFact)
-    suspend fun clearHistory()
+    fun getHistory(): MutableList<NumberFact>
+    fun getFactById(id: Int): NumberFact?
+    fun addFactToHistory(item: NumberFact)
+    fun clearHistory()
 }
 
 class NumberFactRepositoryImpl(private val numberFactDao: NumberFactDao) : NumberFactRepository {
-    override suspend fun getHistory(): List<NumberFact> =
-        numberFactDao.getFactHistory().map { it.toNumberFact() }
 
-    override suspend fun getFactById(id: Int): NumberFact =
-        numberFactDao.getFactById(id).toNumberFact()
-
-    override suspend fun addFactToHistory(item: NumberFact) {
-        numberFactDao.addToHistory(
-            FactItem(
-                number = item.number,
-                text = item.fact
-            )
-        )
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var history: MutableList<NumberFact> = mutableListOf()
+    private var fact: NumberFact? = null
+    override fun getHistory(): MutableList<NumberFact> {
+        scope.launch {
+            async {
+                history = numberFactDao.getFactHistory()
+                    .map { it.toNumberFact() }
+                    .toMutableList()
+            }.await()
+        }
+        return history
     }
 
-    override suspend fun clearHistory() {
-        numberFactDao.clearHistory()
+    override fun getFactById(id: Int): NumberFact? {
+        scope.launch {
+            async {
+                fact = numberFactDao.getFactById(id).toNumberFact()
+            }.await()
+        }
+        return fact
+    }
+
+    override fun addFactToHistory(item: NumberFact) {
+        scope.launch {
+            numberFactDao.addToHistory(
+                FactItem(
+                    number = item.number,
+                    text = item.fact
+                )
+            )
+            history.add(item)
+        }
+    }
+
+    override fun clearHistory() {
+        scope.launch {
+            numberFactDao.clearHistory()
+        }
     }
 }
