8d41cf7c46c6747e6a968faafea20e754b8f2882
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-07 17:48:14 +0200

    Refactoring

--- app/src/main/java/com/example/funnumberfacts/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/funnumberfacts/ui/screen/home/HomeScreen.kt
@@ -1,23 +1,10 @@
 package com.example.funnumberfacts.ui.screen.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.funnumberfacts.R
-import com.example.funnumberfacts.ui.component.NumberFactButton
-import com.example.funnumberfacts.ui.component.NumberTextField
 
 @Composable
 fun HomeScreen(
@@ -32,22 +19,14 @@ fun HomeScreen(
         text = text,
         isValidInput = isValidInput,
         onAction = { action ->
-            handleScreenAction(action, viewModel)
+            when (action) {
+                is HomeScreenAction.OnNumberEntered -> viewModel.onNumberEntered(action.number)
+                HomeScreenAction.OnGetNumberFactClick -> viewModel.onGetNumberFactClick()
+                HomeScreenAction.OnGetRandomNumberFactClick -> viewModel.onGetRandomNumberFactClick()
+                HomeScreenAction.OnHistoryItemClick -> {
+//                    todo: navigate to history screen
+                }
+            }
         }
     )
 }
-
-private fun handleScreenAction(
-    action: HomeScreenAction,
-    viewModel: HomeViewModel
-) {
-    when (action) {
-        is HomeScreenAction.OnNumberEntered -> viewModel.onNumberEntered(action.number)
-        HomeScreenAction.OnGetNumberFactClick -> viewModel.onGetNumberFactClick()
-        HomeScreenAction.OnGetRandomNumberFactClick -> viewModel.onGetRandomNumberFactClick()
-        HomeScreenAction.OnHistoryItemClick -> {
-//            todo: navigate to history screen
-        }
-    }
-}
-
