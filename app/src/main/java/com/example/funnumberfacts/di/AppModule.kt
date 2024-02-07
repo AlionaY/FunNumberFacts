1f81dc3cc809b27dbd3c1ecb7df59bc02ce6918b
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-07 12:35:07 +0200

    Setup DI

---
+++ app/src/main/java/com/example/funnumberfacts/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.funnumberfacts.di
+
+import com.example.funnumberfacts.BuildConfig
+import com.example.funnumberfacts.data.ApiUrl
+import com.example.funnumberfacts.network.ApiServiceFactory
+import com.example.funnumberfacts.network.ApiServiceFactoryImpl
+import com.example.funnumberfacts.network.numberservice.NumberRetrofitService
+import com.example.funnumberfacts.network.numberservice.NumberService
+import com.example.funnumberfacts.network.numberservice.NumberServiceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun apiUrl(): ApiUrl = ApiUrl(BuildConfig.REST_URL)
+
+    @Singleton
+    @Provides
+    fun apiServiceFactory(apiUrl: ApiUrl): ApiServiceFactory = ApiServiceFactoryImpl(apiUrl)
+
+    @Singleton
+    @Provides
+    fun provideNumberService(service: NumberRetrofitService): NumberService =
+        NumberServiceImpl(service)
+}
