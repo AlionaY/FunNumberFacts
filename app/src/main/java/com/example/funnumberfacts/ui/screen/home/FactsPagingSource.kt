4602b6ae1112cd0317c7c9b0e25d3f248520a071
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-26 16:05:33 +0200

    Update loading data to paging source

--- app/src/main/java/com/example/funnumberfacts/ui/screen/home/FactsPagingSource.kt
+++ app/src/main/java/com/example/funnumberfacts/ui/screen/home/FactsPagingSource.kt
@@ -1,11 +1,12 @@
 package com.example.funnumberfacts.ui.screen.home
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.funnumberfacts.db.FactItem
 import com.example.funnumberfacts.repository.NumberFactRepository
 
-private const val LIMIT = 10
+private const val LIMIT = 15
 
 class FactsPagingSource(
     private val repository: NumberFactRepository,
@@ -19,13 +20,17 @@ class FactsPagingSource(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, FactItem> {
         val currentOffset = params.key ?: 0
-        val data = repository.getHistory(defaultLimit, currentOffset)
+        val data = repository.getHistory()
 
         val nextKey = when {
             data.isEmpty() -> null
             data.size < defaultLimit -> null
             else -> currentOffset.plus(data.size)
         }
+        Log.d(
+            "$$$",
+            "offset $currentOffset, params key ${params.key}, next key $nextKey, data size ${data.size}"
+        )
 
         val result = try {
             LoadResult.Page(
